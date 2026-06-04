a47d931a632c7efec970992a07472b00a371f56e
vastgoeddirectnl <info@verkoopjehuisdirect.nl>
2026-06-04 15:15:48 +0200

    Update page.jsx

--- app/huis-direct-verkopen/page.jsx
+++ app/huis-direct-verkopen/page.jsx
@@ -1,11 +1,11 @@
 export const metadata = {
-  title: "Huis direct verkopen",
-  description:
-    "Wilt u uw huis direct verkopen zonder makelaar, bezichtigingen of verkoopstress? Vraag vrijblijvend een helder
-     verkoopvoorstel aan via Vastgoed Direct Nederland.",
-  alternates: {
-    canonical: "https://www.verkoopjehuisdirect.nl/huis-direct-verkopen",
-  },
+title: "Huis direct verkopen",
+description: "Huis direct verkopen zonder makelaar of verkoopstress? Vraag vrijblijvend een helder
+verkoopvoorstel aan via Vastgoed Direct Nederland.",
+alternates: {
+canonical: "https://www.verkoopjehuisdirect.nl/huis-direct-verkopen",
+},
+};
   openGraph: {
     title: "Huis direct verkopen? Vrijblijvend voorstel zonder makelaar",
     description:
