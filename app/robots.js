8c41f2c12e203a5d9340d865b24f3096b1efcbdb
vastgoeddirectnl <info@verkoopjehuisdirect.nl>
2026-06-06 09:37:26 +0200

    Update robots.js

--- app/robots.js
+++ app/robots.js
@@ -1,13 +1,10 @@
 export default function robots() {
   return {
-    rules: [
-      {
-        userAgent: "*",
-        allow: "/",
-        disallow: ["/admin", "/admin/", "/api", "/api/"],
-      },
-    ],
+    rules: {
+      userAgent: "*",
+      allow: "/",
+      disallow: ["/admin", "/admin/", "/api", "/api/"],
+    },
     sitemap: "https://www.verkoopjehuisdirect.nl/sitemap.xml",
-    host: "https://www.verkoopjehuisdirect.nl",
   };
 }
